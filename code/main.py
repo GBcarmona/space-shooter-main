d62e268aafe3c6f3f44fdcc074914e5ee6c9df87
Gabriel Carmona <gbcarmona05@gmail.com>
2024-10-25 17:30:36 -0300

    Changing the space ship sprites

--- code/main.py
+++ code/main.py
@@ -1,32 +1,70 @@
 import pygame
+from os.path import join
+import random
+#join is a method to put all together images you want 
+# random = also a method to make random happen =)
 #general setup
 pygame.init()
 
+WIDTH = 1280
+HEIGHT = 720
+
 caption = 'Study space gamee'
 pygame.display.set_caption(caption)
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 running = True
 
 #testing place surface retangle
-surf = pygame.Surface((100,200))
-surf.fill('orange')
-x= 100
 
-space_ship = pygame.image.load('images/space-ship.png')
-# space_ship = pygame.image.load('images/space-ship.png') if you put ..images/ if the images is out of the file it will goes well but it not good to do it
+path = join('images','space_ship.png')
+path_star = join('images','star.png')
 
+stars = pygame.image.load(path_star).convert_alpha()
+space_ship = pygame.image.load(path).convert_alpha()
+#convert or covert_alpha is for pygame can do a better perfomace
+# space_ship = pygame.image.load('images/space-ship.png') if you put '..images/'
+#  if the folder called 'images' is not in the game files will goes on but isn't a good way to make it
 
+def random_star(num_star) :
+    '''
+    This function generates stars inside the window
+    '''
+    positions = []
+    
+    for _ in range(0,num_star) :
+        x = random.randint(0,WIDTH - stars.get_width())
+        y = random.randint(0,HEIGHT - stars.get_height())
+        positions.append((x,y))
+    return positions
+stars_positions = random_star(20)
+#this function ==> an array is set and a loop for is created to generates stars and put then on the array called 
+#positions with the append
+#yield(x,y)
+    
+# the variable called 'yield' generates series of number over time but they
+#  aren't constantly so i am not using it
+# yield: Used to produce a series of values over time. The function can be resumed later.(AI explanation)
+# Generator: A function that uses yield to return values one at a time instead of all at once. 
+
+       
+#OTHER WAY TO MAKE THE STARS POSITIONS :
+# star_position = [(x,y) randint(0,HEIGHT), randint(0,WIDTH) for i in range(20)]
+# in while loop ==> for position in star position : screen.blit(stars,position)
+        
 while running :
     # inside the while loops the game will run
     for event in pygame.event.get() :
         if event.type == pygame.QUIT :
             running = False
-
-    screen.fill('silver')
-    screen.blit(space_ship,(x,150))
-    x += 0.1
+    
+    screen.fill('darkgray')
+    for places in stars_positions:
+        screen.blit(stars,places)
+    screen.blit(space_ship,(100,150))
+    
+    
     
     pygame.display.update()
-    
+# the for loop is to place the stars that are made in the function that is in the variable stars_allplace
 pygame.quit()
